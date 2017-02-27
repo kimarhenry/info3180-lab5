a6c10eb10edfad591bd952d674dec9d9c5f9dfef
kimarhenry <kimarhenry@yahoo.com>
2017-02-27 00:14:05 +0000

    fifth part

--- app/views.py
+++ app/views.py
@@ -66,6 +66,13 @@ def login():
 def load_user(id):
     return UserProfile.query.get(int(id))
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'Bye')
+    return redirect(url_for('home'))
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
